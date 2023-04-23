15c3176cad4eb186c26dd8c7e1c52d7cd8fa20fe
Nikita Ulianov <person@mectruck.com>
2023-04-22 20:31:01 -0700

    it's close

---
+++ ADV-2/ADV-2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ADV_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = getNumbers(100000);
+            Console.WriteLine(benchmark(numbers));
+            Console.WriteLine(MaxXOR(numbers));
+        }
+
+        public static int benchmark(int[] numbers)
+        {
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                { 
+                    if ((numbers[i] ^ numbers[j]) > result) result = numbers[i] ^ numbers[j];
+                }
+            }
+
+            return result;
+        }
+
+        public static int MaxXOR(int[] numbers)
+        {
+            Array.Sort(numbers);
+
+            int biggest = numbers[numbers.Length - 1];
+
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if ((numbers[i] ^ biggest) > result) result = numbers[i] ^ biggest;
+            }
+
+            return result;
+        }
+
+        public static int[] getNumbers(int number)
+        {
+            Random random = new Random();
+            int[] numbers = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                numbers[i] = random.Next(0, number * 10);
+            }
+
+            return numbers;
+        }
+    }
+}
