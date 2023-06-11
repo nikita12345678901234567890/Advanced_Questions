3de1531671777e5e21390eb9bca251921c897463
Nikita Ulianov <person@mectruck.com>
2023-06-10 20:34:37 -0700

    started

---
+++ ADV-20/ADV-20/Benchmark.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV_20
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Benchmark]
+        public byte[] Linear(byte[] input)
+        {
+            byte[] copy = new byte[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                copy[i] = input[i];
+            }
+
+            return copy;
+        }
+
+        [Benchmark]
+        public byte[] Mine(byte[] input)
+        {
+            byte[] copy = new byte[input.Length];
+
+            for (int i = 0; i < input.Length - 1; i += 2)
+            {
+                copy[i] = input[i];
+                copy[i + 1] = input[i + 1];
+            }
+
+            return copy;
+        }
+
+        [Benchmark]
+        public byte[] ACopy(byte[] input)
+        {
+            byte[] copy = new byte[input.Length];
+
+            Array.Copy(input, copy, input.Length);
+
+            return copy;
+        }
+    }
+}
---
+++ ADV-20/ADV-20/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ADV_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //copy array in sub linear time.
+            var summary = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
