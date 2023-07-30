908048b181e24f313132a9b0786a523893543af4
Nikita Ulianov <person@mectruck.com>
2023-07-29 20:04:02 -0700

    brain = dead

---
+++ ADV-200/ADV-200/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace ADV_200
+{
+    //have a struct consrained to only having IComparables.
+    //On one of these fields only you have [sortBy]
+    //have function that orderBys by this field
+
+    public class OrderBy : Attribute
+    {
+        public IComparable thing;
+    }
+
+    public class Cat
+    {
+        int yeeeet;
+        [OrderBy] double fish;
+    }
+
+    public class Fish
+    {
+        [OrderBy] int yeet;
+        int thingy;
+    }
+
+    public class thingy<T> where T : struct
+    {
+        public IEnumerable<T> derp(IEnumerable<T> values)
+        {
+            T yeet = values.First();
+            var fsh = yeet.GetType();
+
+            foreach (var method in fsh.GetMethods())
+            {
+                if (method.GetCustomAttribute(typeof(OrderBy)) != null)
+                {
+                    return values.OrderBy(method);
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
