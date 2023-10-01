7231aa2989d98fcba0c06c4df54bc378bb6bf83e
Nikita Ulianov <person@mectruck.com>
2023-09-30 20:15:42 -0700

    played with reflection

--- ADV-201/ADV-201/Program.cs
+++ ADV-201/ADV-201/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ADV_201
+﻿using System.Reflection;
+
+namespace ADV_201
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int yeet = 2;
 
+            Type type = yeet.GetType();
+            var instance = Activator.CreateInstance(type);
+
+
+            object thing = new Cat();
+
+            Type type1 = thing.GetType();
+
+            //type1.
+        }
+    }
+
+    public class Cat
+    {
+        int age = 2;
+
+        public Cat(int age = 0)
+        {
+            this.age = age;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine(Meow);
         }
     }
 }
