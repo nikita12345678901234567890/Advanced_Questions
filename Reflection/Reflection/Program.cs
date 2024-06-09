b228b7b7d215e913c35f1233b63c41f432b1b5ea
Nikita Ulianov <person@mectruck.com>
2024-06-08 18:28:03 -0700

    made garfield

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        //abstract class cat
+        //if I inherit from cat, must have a constructor that takes in int age
+
+        public abstract class Cat
+        {
+            public Cat()
+            {
+                if(GetType().GetConstructors().Where(x => x.GetParameters().Where(y => y.ParameterType == typeof(int) && y.Name == "age").Count() != 0).Count() == 0) throw new Exception("yo");
+                
+            }
+        }
+
+        public class Garfield : Cat
+        {
+            //public Garfield(int age)
+            //{
+
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            var yeet = new Garfield();
+        }
+    }
+}
